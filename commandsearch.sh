dd9b5898c772491f2f21edff9db76a9d9eb67ca1
ThomasP <123633993+Prudentnavigator@users.noreply.github.com>
2023-03-06 11:11:28 +0100

    Update commandsearch.sh

--- commandsearch.sh
+++ commandsearch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-# commandsearch.sh--searches the short discription of man-pages and cl-commands on your
-#		     system which contain the search-word from the user input. 
-#		     Display's those commands with the short discription from the man-page.
+# commandsearch.sh--searches the short description of the man-pages and cl-commands on your
+#		     system that contain the search-word from the user input. 
+#		     Display's matching commands with the short description from the man-page.
 
 
 # Check if the command whatis is installed.
@@ -14,13 +14,13 @@ if ! [ -x "$(command -v whatis)" ]; then
        exit 1
 fi       
 
-# Generaly cl-commands are in the /bin and /bin directories.
+# Generally cl-commands are installed in the /bin and /sbin directories.
 dpath=/bin
 dpath1=/sbin
 
-# Prompt the user to input a search-word (i.e crypt, network, firewall,...) in
-#	order to display commands that are related to the desired topic.
-echo "		*******************************
+# Prompt the user to input a search-word (i.e crypt, network, firewall,...).
+echo "	
+		*******************************
 		     command_search.py"
 echo "
 		*******************************
@@ -31,8 +31,7 @@ echo "
 		*******************************
 "
 
-# Display the commands that are related to the search topic with the short
-#	discription from the man-pages.
+# Display the commands that are matching the search-word.
 x=$( ls $dpath $dpath1 )
 y=$( whatis $x 2>/dev/null )
 echo "$y" | nl | grep -i "$search" --color
